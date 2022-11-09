76859c7b70dfb30020fab26dc72d0fa827f385b6
shamima <shamimayesmin144@gamil.com>
2022-11-09 16:21:26 +0600

    question added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -43,19 +43,63 @@ const Blog = () => {
 							What is JWT, and how does it work?
 							</summary>
 							<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">
-								Tincidunt ut hac condimentum rhoncus phasellus nostra. Magna
-								porttitor egestas tincidunt neque vehicula potenti.{" "}
+								<strong>JWT</strong> :
+								<br />
+								JWT, or JSON Web Token, is an open standard used to share security information between two parties — a client and a server. Each JWT contains encoded JSON objects, including a set of claims. JWTs are signed using a cryptographic algorithm to ensure that the claims cannot be altered after the token is issued.
+								<br />
+								<strong>How it works</strong> :
+								<br />
+								JWTs differ from other web tokens in that they contain a set of claims. Claims are used to transmit information between two parties. What these claims are depends on the use case at hand. For example, a claim may assert who issued the token, how long it is valid for, or what permissions the client has been granted.
+								<br />
+								A JWT is a string made up of three parts, separated by dots (.), and serialized using base64. In the most common serialization format, compact serialization, the JWT looks something like this: xxxxx.yyyyy.zzzzz.
+
+
 							</p>
 						</details>
 						<details className="w-full border rounded-lg">
 							<summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">
-								Eleifend feugiat sollicitudin laoreet adipiscing bibendum
-								suscipit erat?
+							What is the difference between javascript and NodeJS?
 							</summary>
 							<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">
-								Justo libero tellus integer tincidunt justo semper consequat
-								venenatis aliquet imperdiet. Ultricies urna proin fusce nulla
-								pretium sodales vel magna et massa euismod vulputate sed.{" "}
+								<strong>Jsvascript</strong> :
+								<br />
+								1.Javascript is a programming language that is used for writing scripts on the website. 
+								<br />
+								2.Javascript can only be run in the browsers.
+								<br />
+								3.It is basically used on the client-side.
+								<br />
+								4.Javascript is capable enough to add HTML and play with the DOM. 
+								<br />
+								5.Javascript can run in any browser engine as like JS core in safari and Spidermonkey in Firefox. 
+								<br />
+								6.Javascript is used in frontend development.
+								<br />
+
+								<strong>NodeJS</strong> :
+								<br />
+								1.NodeJS is a Javascript runtime environment.
+								<br />
+								2.We can run Javascript outside the browser with the help of NodeJS.
+								<br />
+								3.It is mostly used on the server-side.
+								<br />
+								4.Nodejs does not have capability to add HTML tags.
+								<br />
+								5.V8 is the Javascript engine inside of node.js that parses and runs Javascript. 
+								<br />
+								6.Nodejs is used in server-side development.
+
+							</p>
+						</details>
+						<details className="w-full border rounded-lg">
+							<summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">
+							How does NodeJS handle multiple requests at the same time?
+							</summary>
+							<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">
+							NodeJS receives multiple client requests and places them into EventQueue. NodeJS is built with the concept of event-driven architecture. NodeJS has its own EventLoop which is an infinite loop that receives requests and processes them. EventLoop is the listener for the EventQueue. 
+							<br />
+							If NodeJS can process the request without I/O blocking then the event loop would itself process the request and sends the response back to the client by itself. But, it is possible to process multiple requests parallelly using the NodeJS cluster module or worker_threads module.
 							</p>
 						</details>
 					</div>
